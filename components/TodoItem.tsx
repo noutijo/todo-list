245a479799ed0827c65afe1e4ec15bc2b1fe1857
Oreol Noumodomg <oreolnoumodong@gmail.com>
2024-04-30 18:17:08 +0100

    make visible todo on tooltip  when hovering

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,6 +1,6 @@
 import { TrashIcon } from "@heroicons/react/24/outline"
 import { useState } from "react"
-
+import { Tooltip } from "react-tooltip"
 interface IProps {
   title: string
   completed: boolean
@@ -34,13 +34,27 @@ export default function TodoItem({
         type="checkbox"
         className="cursor-pointer w-[1.2rem] h-[1.2rem] text-primaryColor bg-gray-100 border-gray-300 rounded focus:ring-primaryColor dark:ring-offset-gray-800 focus:ring-1 dark:bg-gray-700 dark:border-gray-600"
       />
-
-      <p onClick={checkTodo} className="w-full line-clamp-1 font-[500] cursor-pointer">{title}</p>
+      {/* tittle with tooltip start here */}
+      <a
+        onClick={checkTodo}
+        className="w-full line-clamp-1 font-[500] cursor-pointer"
+        data-tooltip-id="my-tooltip"
+        data-tooltip-variant="light"
+        data-tooltip-delay-show={100}
+        data-tooltip-content={title}>
+        {title}
+      </a>
+      <Tooltip
+        id="my-tooltip"
+        arrowColor="#FF8769"
+        className="!max-w-[400px] !ring-1 !ring-gray-200 !text-wrap !h-auto !text-md !font-[500]"
+      />
+      {/* tittle with tooltip end here */}
 
       <div
         onClick={deleteTodo}
         className="ring-1 ring-primaryColor p-1 rounded-full">
-        <TrashIcon className="h-[14px] w-[14px]  text-primaryColor cursor-pointer" />
+        <TrashIcon className="h-[14px] w-[14px] text-primaryColor cursor-pointer" />
       </div>
     </div>
   )

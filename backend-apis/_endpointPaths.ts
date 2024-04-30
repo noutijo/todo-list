fadbeb5af4378590ab6af77de9378876a67fd8b5
Oreol Noumodomg <oreolnoumodong@gmail.com>
2024-04-30 12:42:01 +0100

    refactoring

--- backend-apis/_endpointPaths.ts
+++ backend-apis/_endpointPaths.ts
@@ -1,3 +1,3 @@
-const BASE_URL = "https://jsonplaceholder.typicode.com/todos"
+const BASE_URL = "https://jsonplaceholder.typicode.com"
 
-export const BASE_API = `${BASE_URL}`
+export const BASE_API = `${BASE_URL}/todos`

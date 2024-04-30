8bd6d1091cac5f321f5357124dd04302702fef6c
Oreol Noumodomg <oreolnoumodong@gmail.com>
2024-04-30 01:25:12 +0100

    small changes and refactoring #2

--- components/AddTodo.tsx
+++ components/AddTodo.tsx
@@ -34,7 +34,7 @@ export default function AddTodo({ onAddTodo }: IProps) {
         disabled={title == "" ? true : false}
         onClick={addTodo}
         className={`${
-          title == "" ? "opacity-75" : ""
+          title == "" ? "opacity-70" : ""
         } w-full md:w-fit px-4 py-2 bg-primaryColor text-white rounded-full`}>
         Add
       </button>

ac7beb1c706a3ebf3a7e2fa8933aa1c639d083fa
Oreol Noumodomg <oreolnoumodong@gmail.com>
2024-05-01 13:07:58 +0100

    add animation ti title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { DM_Sans } from "next/font/google"
 import "../styles/globals.css"
 import "react-toastify/dist/ReactToastify.css"
+import "animated-text-letters/index.css"
 
 import { ToastContainer } from "react-toastify"
 
